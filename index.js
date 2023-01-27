36d283a6d2fc36ba4738b9947dd71f8e61c45e2d
Rahat393 <rahataminulislam37@gmail.com>
2023-01-26 22:48:06 -0800

    apppointment options added

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -7,6 +9,29 @@ const app = express();
 // middleware
 app.use(cors());
 app.use(express.json())
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lnoy20s.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+//  console.log(uri);
+
+ async function run() {
+  try{
+      const appointmentOptionCollection = client.db('doctors-portal2').collection('AppointmentOptions')
+
+      app.get('/appointmentopptions', async(req, res) => {
+        const query = {};
+        const options = await appointmentOptionCollection.find(query).toArray();
+        res.send(options)
+      })
+  }
+  finally{
+
+  }
+ }
+ run().catch(console.log);
+
 
 app.get('/', async(req, res) => {
   res.send('doctors portal surver running')
